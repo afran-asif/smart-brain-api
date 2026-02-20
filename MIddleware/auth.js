5de503d6d422be5bcaf2d62543d3a6c0a474e75e
asif is here <asifafran24@gmail.com>
2026-02-20 06:00:54 +0600

    .....

--- MIddleware/auth.js
+++ MIddleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
-    const { authorization } = req.headers;
+    const { Authorization } = req.headers;
 
-    if (!authorization){
+    if (!Authorization){
         return res.status(401).json({error: 'You must be logged in.'});
     }
-    const token = authorization.replace('Bearer ' , '');
+    const token = Authorization.split(' ')[1];
     
     jwt.verify(token, process.env.JWT_SECRET, (err, payload) =>{
         if(err){
